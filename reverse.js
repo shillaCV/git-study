c14ac471b974cbacd09a2e8a2484c54822eb32bd
Shilla <your-email@gmail.com>
2026-04-21 17:37:57 +0530

    reverse number

--- reverse.js
+++ reverse.js
@@ -1,4 +1,14 @@
+// let word = 'shilla';
+// let reversed = word.split('').reverse().join('');
+// console.log(reversed);
 
-let word = 'shilla';
-let reversed = word.split('').reverse().join('');
-console.log(reversed);
+let num = 12345;
+let reverse = 0;
+let rem;
+
+while (num !== 0) {
+  rem = num % 10;
+  reverse = reverse * 10 + rem;
+  num = Math.floor(num / 10);
+}
+console.log('Reversed number:', reverse);
